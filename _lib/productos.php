7547e1ad6f612e9947dfc8051c9d144cc1b5ab63
Nicolas Quintana <nico123_89@hotmail.com>
2012-05-15 15:38:44 -0300

    Categorias y productos php

--- _lib/productos.php
+++ _lib/productos.php
@@ -3,5 +3,8 @@ $cat1 = array("id"=>"0", "nombre"=>"Resorte de valvulas", "descripcion"=>"Resort
 $cat2 = array("id"=>"1", "nombre"=>"Cinturones de 10 puntas", "descripcion"=>"Cintasdasdasddad de 5 puntas con enganche de aluminio.", "marca"=>"PS", "cantComent"=>"2");
 $cat3 = array("id"=>"2", "nombre"=>"Botitas", "descripcion"=>"Cintasdasdasddad de 5 puntas con enganche de aluminio.", "marca"=>"PS", "cantComent"=>"2");
 $miArray = array("destacado"=>$cat1, $cat2, $cat3);
+
+
+$miArray = array("destacado"=> $cat1,"productos"=>"todosLosProductos");
 echo json_encode($miArray);
 ?>

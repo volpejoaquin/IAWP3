b8d6449c787f7ab02cd0f955f1dddc92139d8d5a
joaco <joaco@joaco-DDELL>
2012-05-15 14:28:08 -0300

    Carga categorias desde php que genera un json

--- _lib/categorias.php
+++ _lib/categorias.php
@@ -1,6 +1,6 @@
 <?php
-echo "{ 
-	id: '0', 
-	nombre: 'Accesorios'
-	}";
+$cat1 = array("id"=>"0", "nombre"=>"Accesorios", "descripcion"=>"Todo tipo de accesorios para el interior de tu auto de carreras. Extractores de volante, redes de puerta	 espejos interiores y exteriores, teclas de contacto para instalacion electrica, etc.", "cantProd"=>"5");
+$cat2 = array("id"=>"1", "nombre"=>"Cintos" , "descripcion"=>"Todo tipo de accesorios para el interior de tu auto de carreras.", "cantProd"=>"5");
+$miArray = array($cat1, $cat2);
+echo json_encode($miArray);
 ?>
---
+++ templates/template2/js/cargarCategorias.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	$.getJSON('_lib/categorias.php', function(data) {
+		for (i=0; i<data.length;i++) {
+			categoria = "";
+			id = data[i].id;
+			nombre = data[i].nombre;
+			descripcion = data[i].descripcion;
+			cantProd = data[i].cantProd;
+			
+			categoria += "<article class='post'>\
+							<header>\
+								 <h3>"+nombre+"</h3>\
+							 </header>\
+							 <p>"+descripcion+"</p>\
+							 <footer>\
+							 <span class='author'>Productos: "+ cantProd+"</span>\
+							 <span class='permalink'><a href='#permalink'>Ver Categoria</a></span>\
+							 </footer>\
+							</article>";
+	
+			$("#categorias").append(categoria);
+		}		
+	});	
+});

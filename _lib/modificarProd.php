db37abdd2008e08a9c2d807adabb688049b0b40d
Nicolas Quintana <nico123_89@hotmail.com>
2012-05-26 16:19:08 -0300

    Terminando el admin..

---
+++ _lib/modificarProd.php
@@ -0,0 +1,38 @@
+<?php
+
+try{
+try {
+    $db = new PDO('sqlite:../db/iawp3.sqlite');
+} catch (Exception $e) {
+    die($e);
+}
+
+
+	//Obtengo la categoria
+	$qry = $db->prepare(
+    'SELECT id FROM categorias WHERE nombre=?');
+	$qry->execute(array($_POST['categoria']));
+	
+	$idcat=1;
+	
+	//Modifico el producto
+	$qry = $db->prepare(
+    "UPDATE productos SET nombre=?, descripcion=?, precio=?,stock=?,id_categoria=".$idcat.",marca=?");
+	$qry->execute(array($_POST['name'],$_POST['desc'],$_POST['precio'],$_POST['stock'],$_POST['marca']));
+	
+	//Las tags??...
+
+//echo $response;
+  echo "¡Se modific&oacute; '".$_POST['name']."' satisfactoriamente!";
+  flush();
+
+}
+catch(PDOException $e)
+    {
+    echo $e->getMessage();
+    }
+
+//Cierro la conex a la bd
+$db=null;
+
+?>
--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,16 @@
+//Snippet para remover un warning molesto de Webkit.... 
+(function(){
+    // remove layerX and layerY
+    var all = $.event.props,
+        len = all.length,
+        res = [];
+    while (len--) {
+      var el = all[len];
+      if (el != 'layerX' && el != 'layerY') res.push(el);
+    }
+    $.event.props = res;
+}());
+
 
 $(document).ready(function() {
 	
@@ -6,18 +19,16 @@ $(document).ready(function() {
 	$("#divC").hide();
 	$("#divD").hide();
 	$("#otraCat").hide();
+	$("#dialog-form").hide();
+	dialogoModif();
 
 	cargarCategorias();
 
 	$("#menuAgregar").click(menuAgregarProd);
-	
 	$("#menuModificar").click(menuModificarProd);
-	
 	$("#menuConfigurar").click(menuConfigurar);
-	
 	$("#menuBackup").click(menuBackup);
-
-
+	
 	//Validar el formulario de agregar
 	$('#agregarForm').validate({
 		debug: false,
@@ -53,7 +64,7 @@ $(document).ready(function() {
 			$.post('_lib/agregarProd.php', $("#agregarForm").serialize(), function(data) {
 					$('#divA').toggle('fast');
 					$('#agregarForm').get(0).reset();
-					$('#result').html(data);
+					$('#resultA').html(data);
 				});
 			
 		}
@@ -72,6 +83,12 @@ function menuAgregarProd(){
 function menuModificarProd(){
 	$("#divB").toggle("fast");
 	
+	var isHidden = $('#divB').is(':hidden');
+	
+	if(isHidden!="true")
+		//Hace la consulta a la bd y carga los productos a la tabla
+		mostrarTablaProductos();
+	
 }
 
 function menuConfigurar(){
@@ -84,9 +101,12 @@ function menuBackup(){
 	
 }
 
-//Carga el combobox de categorias
+//Carga el combobox de categorias, en agregar y modif producto
 function cargarCategorias()
 {
+	$("#list").html("");
+	$("#diagCat").html("");
+
 	var url = "_lib/nombresCategorias.php";
 	var optionString="";
 	$.getJSON(url, function(data){
@@ -103,11 +123,11 @@ function cargarCategorias()
 			}
 			
 			$("#list").append(optionString);
+			$("#diagCat").append(optionString);
 		}
 		
 		$("#list").append("<option value="+(data.length+1)+">Otra...</option>");
 	});
-	
 	
 }
 
@@ -119,42 +139,187 @@ function validarCat(){
 	if(selec=="Otra...")
 	{
 		$("#categoria").val("");
-		$("#otraCat").show("fast");
+		$("#otraCat").toggle('fast');
 	}
 	else
 	{
 		$("#categoria").val($("#list option:selected").val());
-		$("#otraCat").hide("fast");
+		$("#otraCat").toggle('fast');
 		$("#otraCat:input").val("");
 	}
 	
 }
 
 
-function agregar()
-{
-	alert("entre a agregar()....");
-
-/*	var array= new Array();
-	var url = "_lib/agregarProd.php?nombre="+$("#nombre").val()+
-								"&descripcion="+$("#descripcion").val()+
-								"&precio="+$("#precio").val()+
-								"&stock="+$("#stock").val()+
-								"&marca="+$("#marca").val()+
-								"&tags="+$("#tags").val();
-								*/
-								
-								
-	if($("#list option:selected").text()=="Otra...")
-	{
-		url+="&nuevaCat="+$("#list option:selected").val();
-	}
-	else
-	{
-		url+="&categoria="+$("#categoria").val();
-	}
-					
-	}
+//Refresca la tabla
+function mostrarTablaProductos(){
 	
+	//Vacio lo que habia antes 
+	$("#tbodyProductos").html("");
+	
+	//Lleno la tabla 
+	$.getJSON('_lib/tablaProductos.php', function(data) {
+		//Una fila por cada producto nuevo
+		for(i=0;i<data.length;i++){
+			
+			var id = data[i].id;
+			var nombre = data[i].nombre;
+			var desc= data[i].desc;
+			var precio= data[i].precio;
+			var stock = data[i].stock;
+			var cat = data[i].cat;
+			var marca = data[i].marca;
+						
+			var fila = "<tr> "+
+							"<td>"+id+"</td>"+
+							"<td> <span class='spanProducto'>"+nombre+"</span></td>"+
+							"<td> <span class='spanProducto'>"+desc+"</span> </td>"+
+							"<td> <span class='spanProducto'>"+precio+"</span> </td>"+
+							"<td> <span class='spanProducto'>"+stock+"</span> </td>"+			
+							"<td> <span class='spanProducto'>"+cat+"</span></td>"+
+							"<td> <span class='spanProducto'>"+marca+"</span> </td>"+
+							"<td><img class='link editProducto' src='img/editIcon.png' /> </td>"+
+							"<td><img class='link deleteProducto' src='img/deleteIcon.png' /></td>"+
+					   "</tr>";
+			
+			
+			$("#tbodyProductos").append(fila);
+		}
+						
+		/*	
+		//lleno las categorias
+		var url = "_lib/nombresCategorias.php";
+			$.getJSON(url, function(data){
+				for(i=0;i<data.length;i++)
+				{
+					if(i==0)
+					{
+						optionString = "<option value="+i+" selected>"+data[i]+"</option>";
+					}
+					else
+					{
+						
+						optionString = "<option value="+i+">"+data[i]+"</option>";
+					}
+					
+					$("#list").append(optionString);
+				}
+			});	
+						
+			*/
+		});
+	
+	
+	//Cuando ya haya cargado todas las filas de la tabla, asigno los oyentes de click
+	$(".editProducto").live('click',function(){
+		
+		var id;
+		id=$(this).parent().parent().children(':first-child').text();
+		
+			$("#dialog-form" ).dialog( "open" );
+		
+		
+	});
+	
+	$(".deleteProducto").live('click',function(){
+		var id;
+		id=$(this).parent().parent().children(':first-child').text();
+	
+		alert("Quiere borrar el producto> "+id);
+	});
+	
+
+}
+	
+//Inicializa el dialog de modificar productos
+function dialogoModif (){
+	
+	var name = $( "#diagName" ),
+			desc = $( "#diagDesc" ),
+			precio = $( "#diagPrecio" ),
+			stock = $( "#diagStock" ),
+			categoria = $( "#diagCat" ),
+			marca = $( "#diagMarca" ),
+			allFields = $( [] ).add( name ).add( desc ).add( precio ).add(stock).add(categoria).add(marca),
+			tips = $( ".validacion" );
+
+		function updateTips( t ) {
+			tips
+				.text( t )
+				.addClass( "ui-state-highlight" );
+			setTimeout(function() {
+				tips.removeClass( "ui-state-highlight", 1500 );
+			}, 500 );
+		}
+
+		function checkLength( o, n, min, max ) {
+			if ( o.val().length > max || o.val().length < min ) {
+				o.addClass( "ui-state-error" );
+				updateTips( "El tamaño de " + n + " debe ser entre " +
+					min + " y " + max + " caracteres." );
+				return false;
+			} else {
+				return true;
+			}
+		}
+
+		function checkRegexp( o, regexp, n ) {
+			if ( !( regexp.test( o.val() ) ) ) {
+				o.addClass( "ui-state-error" );
+				updateTips( n );
+				return false;
+			} else {
+				return true;
+			}
+		}
+		
+		$( "#dialog-form" ).dialog({
+			autoOpen: false,
+			height: 300,
+			width: 350,
+			modal: true,
+			buttons: {
+				"Modificar producto": function() {
+					var bValid = true;
+					allFields.removeClass( "ui-state-error" );
+
+					bValid = bValid && checkLength( name, "Nombre", 1, 30 );
+					bValid = bValid && checkLength( desc, "Descripción", 1, 80 );
+					bValid = bValid && checkLength( precio, "Precio", 1, 20 );
+					bValid = bValid && checkLength( stock, "Stock", 1, 10 );
+					bValid = bValid && checkLength( marca, "Marca", 1, 30 );
+
+					bValid = bValid && checkRegexp( name, /^[a-z|A-Z]([0-9a-z_A-Z ])+$/i, "El nombre del producto sólo puede contener letras, números y guiones bajos, empezando con una letra." );
+					bValid = bValid && checkRegexp( desc, /^[a-z|A-Z]([0-9a-z_A-Z ])+$/i, "La descripción del producto sólo puede contener letras, números y guiones bajos, empezando con una letra." );
+              		//bValid = bValid && checkRegexp( precio,/^  $/i, "El precio debe ser un número con dos cifras decimales, separar con punto o coma es indistinto");
+					bValid = bValid && checkRegexp( stock, /^([0-9]|[1-9][0-9]+)$/i, "El stock debe ser un número entero." );
+					bValid = bValid && checkRegexp( marca, /^[a-z|A-Z]([0-9a-z_A-Z ])+$/i, "La marca del producto sólo puede contener letras, números y guiones bajos, empezando con una letra." );
+         
+					
+					if ( bValid ) { //Si se validaron los campos
+						
+						//Modificar el producto en la BD
+						$.get("_lib/modificarProd.php?name="+name.val(), $("#modificarForm").serialize(), function(data) {
+							alert(data);
+						});
+						
+						
+						//Refrescar la tabla
+						modificarTablaProductos();
+						$( this ).dialog( "close" );
+					}
+				},
+				"Cancelar": function() {
+					allFields.val( "" ).removeClass( "ui-state-error" );
+					$( this ).dialog( "close" );
+				}
+			},
+			close: function() {
+				allFields.val( "" ).removeClass( "ui-state-error" );
+			}
+		});
+	
+	
+}
 
 
